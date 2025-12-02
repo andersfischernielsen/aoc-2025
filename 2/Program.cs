807a990437044a5190eb183df2ea8ec570940a92
Anders Fischer-Nielsen <andersfischern@me.com>
2025-12-02 10:04:08 +0100

    Solve Day 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,15 +1,9 @@
 ﻿
 using System.Text.RegularExpressions;
 
-var reader = new StreamReader("input.txt");
-var line = reader.ReadLine();
-var regex = "^(\\d+)\\1{1,}$";
-var matches = new List<long>();
-
 static IEnumerable<long> CreateRange(long start, long count)
 {
     var limit = start + count;
-
     while (start < limit)
     {
         yield return start;
@@ -17,24 +11,24 @@ static IEnumerable<long> CreateRange(long start, long count)
     }
 }
 
+var reader = new StreamReader("input.txt");
+var line = reader.ReadLine();
+var regex = "^(\\d+)\\1{1,}$";
+var matches = new List<long>();
+
 while (line != null)
 {
     var ranges = line.Split(',');
     foreach (var raw in ranges)
     {
-        if (raw == "") continue;
+        if (raw.IsWhiteSpace()) continue;
 
         var parts = raw.Split("-");
         var start = long.Parse(parts[0]);
         var end = long.Parse(parts[1]);
-        var range = CreateRange(start, end - start + 1);
-        foreach (var number in range)
-        {
-            if (Regex.IsMatch(number.ToString(), regex, RegexOptions.IgnoreCase))
-            {
-                matches.Add(number);
-            }
-        }
+        var matching = CreateRange(start, end - start + 1)
+            .Where(n => Regex.IsMatch(n.ToString(), regex, RegexOptions.IgnoreCase));
+        matches.AddRange(matching);
     }
     line = reader.ReadLine();
 }
